968b2a61720d03a4043911bf879efb365899b3b6
Somesh Kumar <someshkumar0805@gmail.com>
2026-09-22 21:31:37 +0530

    resolve some issues regarding database connectioin

--- mongodb.js
+++ mongodb.js
@@ -3,9 +3,9 @@ const mongodb = require('mongodb');
 const dbClient = mongodb.MongoClient;
 
 let _db;
-const URL = "mongodb+srv://samsomesh2o2o_db_user:QZHnwKc4cEPdQ98j@cluster-1.de1mykq.mongodb.net/?appName=Cluster-1";
 
 const mongoCon = (callback) => {
+  const URL = process.env.MONGO_URL;
   dbClient.connect(URL)
   .then(client => {
     console.log('connected to MongoDB')
